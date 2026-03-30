0e64e94d1cf1ac7bd846c7a0d04363606ea50a58
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 13:20:01 +0300

    Update views.py

--- suppliers/views.py
+++ suppliers/views.py
@@ -1,27 +1,7 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from .models import Product, Supplier
-from django.contrib.auth.models import User
+from django.contrib import admin
+from django.urls import path, include
 
-def supplier_login(request):
-    if request.method == 'POST':
-        u = request.POST.get('username')
-        p = request.POST.get('password')
-        
-        if not User.objects.filter(username=u).exists():
-            messages.error(request, "الحساب غير مسجل في المنصة")
-        else:
-            user = authenticate(username=u, password=p)
-            if user:
-                login(request, user)
-                return redirect('suppliers:dashboard')
-            else:
-                messages.error(request, "كلمة المرور خطأ")
-    return render(request, 'suppliers/login.html')
-
-@login_required
-def supplier_dashboard(request):
-    draft_count = Product.objects.filter(supplier=request.user.supplier, status='draft').count()
-    return render(request, 'suppliers/dashboard.html', {'draft_count': draft_count})
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('suppliers.urls')), # توجيه الصفحة الرئيسية لتطبيق الموردين
+]
