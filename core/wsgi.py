7d7d52d90fb5e9b1a5a7faae24dae9f1b4bca181
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 13:11:08 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,11 +3,10 @@ import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# الحصول على المسار المطلق للمجلد الحالي وإضافته لـ sys.path
+# إجبار النظام على رؤية المجلد الرئيسي
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append(str(BASE_DIR))
+sys.path.insert(0, str(BASE_DIR))
 
-# تحديد ملف الإعدادات
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
