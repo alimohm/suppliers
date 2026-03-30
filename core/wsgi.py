c248e99a9fa07b8ce7b976e3a5b24bf98934de53
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 13:02:59 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,8 +1,13 @@
 import os
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# إعداد ملف الإعدادات الافتراضي للمشروع
+# إضافة مسار المشروع لبيئة التشغيل
+path_root = Path(__file__).resolve().parent.parent
+if str(path_root) not in sys.path:
+    sys.path.append(str(path_root))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# هذا هو المتغير الذي يبحث عنه Gunicorn (يجب أن يكون اسمه application)
 application = get_wsgi_application()
