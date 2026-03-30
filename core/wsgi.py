db2c6332243a4b397b5e9612a751e0929a85cf42
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 13:26:26 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,10 +3,9 @@ import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# إضافة مسار المشروع لبيئة بايثون
+# إجبار النظام على رؤية المجلد الرئيسي
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append(str(BASE_DIR))
+sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = get_wsgi_application()
