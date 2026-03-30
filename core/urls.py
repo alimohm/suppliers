0d73d6b4f6ac8644f853b888470fb6f52e44bc46
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 16:59:30 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
-# تخصيص نصوص لوحة التحكم لتناسب "سوقك الذكي"
-admin.site.site_header = "Mahjoub Online - Smart Market"
-admin.site.site_title = "لوحة تحكم الموردين"
-admin.site.index_title = "إدارة سلاسل التوريد الرقمية"
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # الرابط الذي ستفتحه هو /dashboard/
-    path('dashboard/', admin.site.urls), 
-    path('suppliers/', include('suppliers.urls')),
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('dashboard/', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
+    path('', auth_views.LoginView.as_view(template_name='registration/login.html')), # الصفحة الأولى هي الدخول
 ]
