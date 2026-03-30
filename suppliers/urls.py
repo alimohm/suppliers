8f4b31110ce09c0103006569421faadff9086043
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 12:49:32 +0300

    Update urls.py

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,10 +1,9 @@
-
 from django.urls import path
 from . import views
 
 app_name = 'suppliers'
 
 urlpatterns = [
-    # رابط صفحة الدخول: سيصبح your-domain.com/suppliers/login/
     path('login/', views.supplier_login, name='login'),
+    path('dashboard/', views.supplier_dashboard, name='dashboard'),
 ]
