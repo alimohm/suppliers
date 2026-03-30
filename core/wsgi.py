e6ddefa49bbafbee5ef769ccc46013d1058d906d
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 13:36:28 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,9 +3,8 @@ import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# الحصول على المسار المطلق للمجلد الرئيسي
+# إجبار النظام على رؤية المجلد الرئيسي للمشروع
 BASE_DIR = Path(__file__).resolve().parent.parent
-# إضافته إلى قائمة البحث الخاصة ببايثون
 sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
