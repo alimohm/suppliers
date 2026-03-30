f5acab812d86ef8699e38c0e1e9ab4b9cd127e1f
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 18:42:26 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from suppliers.views import dashboard
+from django.shortcuts import redirect
+
+# وظيفة لتحويل الزائر غير المسجل للدخول
+def root_redirect(request):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+    return redirect('/dashboard/')
 
 urlpatterns = [
-    path('admin/', admin.site.urls), #
-    path('login/', include('django.contrib.auth.urls')), # تفعيل صفحة الدخول
-    path('dashboard/', dashboard, name='dashboard'), # تفعيل لوحة الرفع
-    path('', dashboard), # الصفحة الرئيسية
+    path('admin/', admin.site.urls),
+    path('login/', include('django.contrib.auth.urls')), 
+    path('dashboard/', dashboard, name='dashboard'),
+    path('', root_redirect), # تحويل ذكي عند فتح الرابط الرئيسي
 ]
