5cd64dc97eede91c6cf2ed7400762b3c53c35664
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-31 23:31:30 +0300

    Update finance.py

--- finance.py
+++ finance.py
@@ -1,7 +1,5 @@
-def calculate_final_price(cost, currency="USD"):
-    """منطق زيادة الـ 30% الملكية"""
+def calculate_final_price(cost):
+    """حسبة ملكية بزيادة 30%"""
     try:
-        price = float(cost)
-        return round(price * 1.30, 2)
-    except:
-        return 0.0
+        return round(float(cost) * 1.30, 2)
+    except: return 0.0
