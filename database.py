39dcc6ebfe8a20e133f34feed27cb9d08b0303d1
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-31 13:49:28 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,22 @@
 
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def init_db(app):
+    db_url = os.environ.get('DATABASE_URL')
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+class Vendor(db.Model):
+    __tablename__ = 'vendor'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    owner_name = db.Column(db.String(100))
+    wallet_address = db.Column(db.String(100))
