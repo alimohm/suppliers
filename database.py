222c43a1db1d04c42246bd944c5c5937b7cf062c
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-31 20:20:03 +0300

    Update database.py

--- database.py
+++ database.py
@@ -7,17 +7,14 @@ def init_db(app):
     db_url = os.environ.get('DATABASE_URL')
     if db_url and db_url.startswith("postgres://"):
         db_url = db_url.replace("postgres://", "postgresql://", 1)
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or 'sqlite:///local_dev.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
 class Vendor(db.Model):
     __tablename__ = 'vendor'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    # أعمدة إضافية ضرورية للهيكل الحقيقي
-    owner_name = db.Column(db.String(100))
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(100))
+    owner_name = db.Column(db.String(100)) # تأكد من إضافة هذا العمود في ريلوي
     wallet_address = db.Column(db.String(200))
-    store_name = db.Column(db.String(100))
