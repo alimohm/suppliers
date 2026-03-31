5acfb9ca383b1689abcb52d0db83a906eb424342
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-31 21:53:16 +0300

    Update finance.py

--- finance.py
+++ finance.py
@@ -3,9 +3,9 @@ from config import Config
 def calculate_final_price(raw_price, currency):
     try:
         price = float(raw_price)
-        # تحويل الدولار للريال
+        # التحويل للريال إذا كانت العملة دولار
         base_price = price * Config.USD_TO_SAR if currency == "USD" else price
-        # إضافة عمولة 30%
+        # إضافة الربح 30%
         return round(base_price * Config.PROFIT_MARGIN, 2)
     except:
-        return 0
+        return 0.0
