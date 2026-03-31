efcd04acb388d532653215e9920b52d7c997c159
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-31 23:31:51 +0300

    Update bridge_logic.py

--- bridge_logic.py
+++ bridge_logic.py
@@ -1,10 +1,3 @@
-import requests
-
 def push_to_store(data):
-    """التحقق من الاتصال بالويب هوك"""
-    # هنا يتم وضع رابط الويب هوك الخاص بمتجرك
-    try:
-        # محاكاة نجاح الاتصال للتحقق
-        return "success" 
-    except:
-        return "connection_error"
+    """منطق التحقق من الاتصال بالمتجر"""
+    return "success" # حالة افتراضية للنجاح
