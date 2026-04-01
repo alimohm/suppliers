57ce60e466c9a8a917210874d72bfe2f0212f31e
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-01 12:49:45 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,8 +1,9 @@
 # logic.py
 from database import Vendor
 
-def do_login_check(username, password): # غيرنا الاسم هنا لتجنب التعارض
+def execute_authentication(username, password): 
+    """دالة التحقق المستقلة تماماً"""
     user = Vendor.query.filter_by(username=username).first()
     if user and user.password == password:
         return {"status": True, "user": user}
-    return {"status": False, "message": "بيانات غير صحيحة"}
+    return {"status": False, "message": "بيانات الدخول غير صحيحة"}
