290b527e977e067454dba063b4a8dc4d35a811bf
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-02 20:47:38 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,1 @@
-# ملف التجميع والربط __init__.py
 
-from .database import db, init_db
-from .models import Vendor, Product, generate_mah_wallet
-from .logic import login_vendor, logout, is_logged_in
-from .sync_service import send_to_qumra_webhook
-
-# الآن أي ملف آخر في المشروع يستطيع استدعاء هذه الأدوات بساطة
-# مثال: from . import db, Vendor
