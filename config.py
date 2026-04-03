9f689490d4d7a378125805e5c0a7c4a9179d1855
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-03 21:00:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 import os
 
 class Config:
-    # مفتاح التشفير للأمان
     SECRET_KEY = os.environ.get('SECRET_KEY', 'mahjoub_secret_key_2026')
-    
-    # جلب رابط قاعدة البيانات من بيئة Railway
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
-    # تصحيح الرابط ليتوافق مع SQLAlchemy
+    # تصحيح رابط قاعدة البيانات ليتوافق مع SQLAlchemy
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     
